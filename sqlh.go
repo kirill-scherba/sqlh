2272abf062844f4896494e187d04ea93756d3042
Kirill Scherba <kirill@scherba.ru>
2024-09-21 13:53:52 +0300

    Create Update func; closes #18

--- sqlh.go
+++ sqlh.go
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-var ErrValueIsNotStruct = fmt.Errorf("value is not a struct")
+var ErrTypeIsNotStruct = fmt.Errorf("type is not a struct")
 
 // Table returns a SQL CREATE TABLE statement for the given struct type.
 //
@@ -36,53 +36,46 @@ var ErrValueIsNotStruct = fmt.Errorf("value is not a struct")
 //   - db_type:"text" - set database field type
 //   - db_key:"not null primary key" - set database field key
 func Table[T any]() (string, error) {
-	t := reflect.TypeOf(new(T)).Elem()
-	if t.Kind() != reflect.Struct {
-		return "", ErrValueIsNotStruct
+
+	// Check if type is struct
+	if err := checkType[T](); err != nil {
+		return "", err
 	}
 
-	name := strings.ToLower(t.Name())
+	t := reflect.TypeOf(new(T)).Elem()
+
 	var dbFields []string
 	for i := 0; i < t.NumField(); i++ {
+
 		field := t.Field(i)
-		fieldName, ok := getFieldName(t.Field(i))
+
+		// Get field name
+		fieldName, ok := getFieldName(field)
 		if !ok {
 			continue
 		}
 
-		dbType := field.Tag.Get("db_type")
-		if dbType == "" {
-			switch field.Type.Kind() {
-			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-				// Sql does not support all integer types, so we map them all to "integer"
-				dbType = "integer"
-			case reflect.Uint8:
-				dbType = "tinyint"
-			case reflect.Uint, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-				dbType = "bigint"
-			case reflect.Float32, reflect.Float64:
-				dbType = "double"
-			case reflect.Bool:
-				dbType = "bit"
-			case reflect.String:
-				dbType = "text"
-			default:
-				// If the type is not supported, return an error
-				return "", fmt.Errorf("unsupported type: %s", field.Type.Kind())
-			}
+		// Get field type
+		fieldType, err := getFieldType(field)
+		if err != nil {
+			return "", err
 		}
+
 		dbFields = append(dbFields,
 			strings.TrimRight(
 				// Remove trailing spaces from the string
-				fmt.Sprintf("%s %s %s", strings.ToLower(fieldName), dbType,
+				fmt.Sprintf("%s %s %s", strings.ToLower(fieldName), fieldType,
 					field.Tag.Get("db_key")),
 				" ",
 			),
 		)
 	}
 
-	return fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (%s);", name,
-		strings.Join(dbFields, ", ")), nil
+	// Return CREATE TABLE statement
+	return fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (%s);",
+		name[T](),
+		strings.Join(dbFields, ", "),
+	), nil
 }
 
 // Insert returns a SQL INSERT statement for the given struct type.
@@ -92,22 +85,46 @@ func Table[T any]() (string, error) {
 // field name. The returned string is a SQL statement that can be executed
 // directly.
 func Insert[T any]() (string, error) {
-	t := reflect.TypeOf(new(T)).Elem()
-	if t.Kind() != reflect.Struct {
-		return "", ErrValueIsNotStruct
+
+	// Check if type is struct
+	if err := checkType[T](); err != nil {
+		return "", err
 	}
 
-	fields := make([]string, 0, t.NumField())
-	for i := 0; i < t.NumField(); i++ {
-		if fieldName, ok := getFieldName(t.Field(i)); ok {
-			fields = append(fields, fieldName)
-		}
-	}
+	// Get table field names
+	fields := fields[T]()
 
+	// Return INSERT statement
 	return fmt.Sprintf("INSERT INTO %s(%s) VALUES(%s);",
-		strings.ToLower(t.Name()),
+		name[T](),
 		strings.Join(fields, ","),
 		strings.TrimRight(strings.Repeat("?,", len(fields)), ","),
+	), nil
+}
+
+// Update returns a SQL UPDATE statement for the given struct type.
+func Update[T any](wheres ...string) (string, error) {
+
+	// Check if type is struct
+	if err := checkType[T](); err != nil {
+		return "", err
+	}
+
+	// Get field names
+	fields := fields[T]()
+
+	// Where clause should be set
+	if len(wheres) == 0 {
+		return "", fmt.Errorf(
+			"where clause should be set in the Update statement",
+		)
+	}
+
+	// Return UPDATE statement
+	return fmt.Sprintf("UPDATE %s SET %s WHERE %s;",
+		name[T](),
+		strings.Join(fields, "=?,")+"=?",
+		strings.Join(wheres, "=? AND ")+"=?",
 	), nil
 }
 
@@ -132,6 +149,11 @@ type Paginator struct {
 // The wheres parameter is an optional list of where clauses. If specified, the
 // where clauses will be joined with " and " and added to the SQL statement.
 func Select[T any](attr *SelectAttr) (string, error) {
+
+	// Check if type is struct
+	if err := checkType[T](); err != nil {
+		return "", err
+	}
 
 	// Make where clause and offset limit from attr struct
 	var where string
@@ -158,8 +180,12 @@ func Select[T any](attr *SelectAttr) (string, error) {
 	}
 
 	// Return the complete SELECT statement
-	name := strings.ToLower(reflect.TypeOf(new(T)).Elem().Name())
-	return fmt.Sprintf("SELECT * from %s%s%s%s;", name, where, orderby, limit), nil
+	return fmt.Sprintf("SELECT * from %s%s%s%s;",
+		name[T](),
+		where,
+		orderby,
+		limit,
+	), nil
 }
 
 // Count returns a SQL COUNT statement for the given struct type.
@@ -172,6 +198,11 @@ func Select[T any](attr *SelectAttr) (string, error) {
 // The wheres parameter is an optional list of where clauses. If specified, the
 // where clauses will be joined with " and " and added to the SQL statement.
 func Count[T any](attr *SelectAttr) (string, error) {
+
+	// Check if type is struct
+	if err := checkType[T](); err != nil {
+		return "", err
+	}
 
 	// Make where clause and offset limit from attr struct
 	var where string
@@ -186,8 +217,7 @@ func Count[T any](attr *SelectAttr) (string, error) {
 	}
 
 	// Return the complete SELECT statement
-	name := strings.ToLower(reflect.TypeOf(new(T)).Elem().Name())
-	return fmt.Sprintf("SELECT count(*) from %s%s;", name, where), nil
+	return fmt.Sprintf("SELECT count(*) from %s%s;", name[T](), where), nil
 }
 
 // Delete returns a SQL DELETE statement for the given struct type.
@@ -200,13 +230,17 @@ func Count[T any](attr *SelectAttr) (string, error) {
 // The wheres parameter is an optional list of where clauses. If specified, the
 // where clauses will be joined with " and " and added to the SQL statement.
 func Delete[T any](wheres ...string) (string, error) {
+
+	// Check if type is struct
+	if err := checkType[T](); err != nil {
+		return "", err
+	}
+
 	// Join the where statements with " and "
 	var where string
 	if len(wheres) > 0 {
 		where = strings.Join(wheres, " and ")
 	}
-
-	name := strings.ToLower(reflect.TypeOf(new(T)).Elem().Name())
 
 	// Add the where statement to the SQL query
 	if len(where) > 0 {
@@ -214,7 +248,7 @@ func Delete[T any](wheres ...string) (string, error) {
 	}
 
 	// Return the complete DELETE statement
-	return fmt.Sprintf("DELETE from %s%s;", name, where), nil
+	return fmt.Sprintf("DELETE from %s%s;", name[T](), where), nil
 }
 
 // Args returns the arguments array for the given struct type.
@@ -227,10 +261,12 @@ func Args(row any) ([]interface{}, error) {
 	rowVal := reflect.ValueOf(row)
 	rowType := reflect.TypeOf(row)
 
+	// Check if row is struct
 	if rowVal.Kind() != reflect.Struct {
-		return nil, ErrValueIsNotStruct
+		return nil, ErrTypeIsNotStruct
 	}
 
+	// Make arguments array for the given struct
 	args := make([]interface{}, 0, rowVal.NumField())
 	for i := 0; i < rowVal.NumField(); i++ {
 
@@ -238,9 +274,11 @@ func Args(row any) ([]interface{}, error) {
 		if rowType.Field(i).Tag.Get("db") == "-" {
 			continue
 		}
+	
 		arg := rowVal.Field(i).Interface()
 		args = append(args, &arg)
 	}
+
 	return args, nil
 }
 
@@ -257,11 +295,12 @@ func ArgsAppay(row any, args []interface{}) (err error) {
 
 	// Check if the given value is a struct
 	if rowVal.Kind() != reflect.Struct {
-		return ErrValueIsNotStruct
+		return ErrTypeIsNotStruct
 	}
 
 	// Loop through the struct fields
 	for i := 0; i < rowVal.NumField(); i++ {
+		
 		// Skip not db fields tagged with "-"
 		if rowType.Field(i).Tag.Get("db") == "-" {
 			continue
@@ -301,7 +340,32 @@ func ArgsAppay(row any, args []interface{}) (err error) {
 	return
 }
 
-// GetFieldName returns a SQL fields name using db tag.
+// checkType checks type T is struct.
+func checkType[T any]() (err error) {
+	t := reflect.TypeOf(new(T)).Elem()
+	if t.Kind() != reflect.Struct {
+		err = ErrTypeIsNotStruct
+	}
+	return
+}
+
+// name returns table name from struct name.
+func name[T any]() string {
+	return strings.ToLower(reflect.TypeOf(new(T)).Elem().Name())
+}
+
+// fields returns a list of struct field names.
+func fields[T any]() (fields []string) {
+	t := reflect.TypeOf(new(T)).Elem()
+	for i := 0; i < t.NumField(); i++ {
+		if fieldName, ok := getFieldName(t.Field(i)); ok {
+			fields = append(fields, fieldName)
+		}
+	}
+	return
+}
+
+// getFieldName returns a SQL fields name using db tag.
 func getFieldName(field reflect.StructField) (fieldName string, ok bool) {
 	fieldName = field.Tag.Get("db")
 	switch fieldName {
@@ -311,5 +375,33 @@ func getFieldName(field reflect.StructField) (fieldName string, ok bool) {
 		return
 	}
 	ok = true
+	return
+}
+
+// getFieldType returns a SQL field type using db_type tag.
+func getFieldType(field reflect.StructField) (fieldType string, err error) {
+
+	fieldType = field.Tag.Get("db_type")
+	if fieldType == "" {
+		switch field.Type.Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			// Sql does not support all integer types, so we map them all to "integer"
+			fieldType = "integer"
+		case reflect.Uint8:
+			fieldType = "tinyint"
+		case reflect.Uint, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			fieldType = "bigint"
+		case reflect.Float32, reflect.Float64:
+			fieldType = "double"
+		case reflect.Bool:
+			fieldType = "bit"
+		case reflect.String:
+			fieldType = "text"
+		default:
+			// If the type is not supported, return an error
+			return "", fmt.Errorf("unsupported type: %s", field.Type.Kind())
+		}
+	}
+
 	return
 }
